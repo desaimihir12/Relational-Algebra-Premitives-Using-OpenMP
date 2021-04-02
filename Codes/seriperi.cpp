6ad7a26eddf55567404cff01f6a85e0f4ca767e3
Mihir <desaimihir12@gmail.com>
2021-04-02 14:50:15 +0530

    codes updated

--- Codes/seriperi.cpp
+++ Codes/seriperi.cpp
@@ -51,18 +51,45 @@ int main() {
 
     fout << "Graph_Name,Vertices,Edges,Edges_in_TC,Run_time,num_procs\n";
     vector<string> file_name;
-    file_name.pb("Tina_AskCal.csv");
-    //file_name.pb("GD02_a.csv");
-    //file_name.pb("ibm32.csv");
-    //file_name.pb("GD95_c.csv");
-    //file_name.pb("GD02_b.csv");
-    //file_name.pb("GD97_a.csv");
-    //file_name.pb("GD98_b.csv");
-    //file_name.pb("GD96_d.csv");
-    
+    file_name.pb("Tina_AskCal.csv");   //11
+    file_name.pb("GD02_a.csv");        //23
+    file_name.pb("ibm32.csv");         //32
+    file_name.pb("GD95_c.csv");        //62
+    file_name.pb("GD02_b.csv");        //80
+    file_name.pb("GD97_a.csv");        //84
+    file_name.pb("GD98_b.csv");        //121
+    file_name.pb("GD96_d.csv");        //180
+    file_name.pb("Harvard500.csv");    //500
+    file_name.pb("Roget.csv");         //1022
+    file_name.pb("GD06_java.csv");     //1538  
+    file_name.pb("CSphd.csv");         //1882
+    file_name.pb("Kohonen.csv");       //4470
+    file_name.pb("EPA.csv");           //4772
+  
+
+    vector<int> sizeofmat;
+    sizeofmat.pb(11);
+    sizeofmat.pb(23);
+    sizeofmat.pb(32);
+    sizeofmat.pb(62);
+    sizeofmat.pb(80);
+    sizeofmat.pb(84);
+    sizeofmat.pb(121);
+    sizeofmat.pb(180);
+    sizeofmat.pb(500);
+    sizeofmat.pb(1022);
+    sizeofmat.pb(1538);
+    sizeofmat.pb(1882);
+    sizeofmat.pb(4470);
+    sizeofmat.pb(4772);
+
+    int tot_runs = 10;
+    cout << "Enter number of runs: ";
+    cin >>  tot_runs;
 
 for(int itr=0;itr<file_name.size();itr++)
 {
+    
     int64_t tot_time = 0;
 
     vector<int> x,y; //edges from x to y
@@ -110,6 +137,8 @@ for(int itr=0;itr<file_name.size();itr++)
         cnt++;
     }
 
+
+
     // Print initial adjacency list
     if(flag)
     {
@@ -126,8 +155,15 @@ for(int itr=0;itr<file_name.size();itr++)
     }
     }
     
+    vector<int> vertices;
+    for(p = umap.begin();p!=umap.end();p++)
+    {
+        vertices.pb(p->first);
+    }
+    
     //cout << "\nMAX loop iterations = " << cnt << endl;
-int tot_runs = 10;
+    
+
 
 //serial implementation
 for(int run_id=1;run_id<=tot_runs;run_id++)
@@ -185,19 +221,21 @@ for(int run_id=1;run_id<=tot_runs;run_id++)
         cout << "Number of edges of Input graph : " << cntb << endl << 
         "Number of edges of Transitive closure : " << cnta << endl;
 
-        fout << fixed << setprecision(12) << file_name[itr] << ',' << 0 << ',' <<
+        fout << fixed << setprecision(12) << file_name[itr] << ',' << sizeofmat[itr] << ',' <<
         cntb << ',' << cnta <<','<< (tot_time*pow(10,-9))/tot_runs <<','<< 1 << "\n";
 
 
 //parallel implementation
 vector<int> procs;
 procs.pb(2);
-procs.pb(3);
 procs.pb(4);
+procs.pb(8);
+procs.pb(16);
+
 
 int64_t tot_para = 0;
 
-
+int nthreads;
 
 for(int proc_id=0;proc_id<procs.size();proc_id++)
 {
@@ -216,21 +254,24 @@ for(int proc_id=0;proc_id<procs.size();proc_id++)
 
         #pragma omp parallel
         {
-
+        int nthrds,id;
+        id = omp_get_thread_num();
+          nthrds = omp_get_num_threads();
+          if(id==0) nthreads = nthrds;
         for(int k=0;k<cnt;k++)           //no.of vertices
         {
             //parallelize from here
 
             #pragma omp for
-            for(unordered_map<int, set<int>>:: iterator p0 = umap2.begin();p0!=umap2.end();p0++)        //no.of vertices
+            for(int p0=0;p0<vertices.size();p0++)        //no.of vertices
             {
                 set<int>::iterator itr;
-                for(itr = p0->second.begin();itr != p0->second.end();itr++)    //length of adjacency list -> no.of vertices
+                for(itr = umap2[vertices[p0]].begin();itr != umap2[vertices[p0]].end();itr++)    //length of adjacency list -> no.of vertices
                 {
                     set<int>::iterator itr2;
                     for(itr2 = umap[*itr].begin();itr2!=umap[*itr].end();itr2++)    //length of adjacency list -> no.of vertices
                     {
-                        p0->second.insert(*itr2);
+                        umap2[vertices[p0]].insert(*itr2);
                     }
                 }
             }
@@ -257,13 +298,12 @@ for(int proc_id=0;proc_id<procs.size();proc_id++)
         }
 
         
-        cout << "\n\nFile : " << file_name[itr] << endl;
-        cout << fixed << setprecision(12) << "Average Paralle Run-time for P = "<<procs[proc_id]<<" = " << 
+        cout << fixed << setprecision(12) << "Average Parallel Run-time for P = "<<nthreads<<" ==>> " << 
         (tot_time*pow(10,-9))/tot_runs  << " second"<< endl;
 
 
-        fout << fixed << setprecision(12) << file_name[itr] << ',' << 0 << ',' <<
-        cntb << ',' << cnta <<','<< (tot_time*pow(10,-9))/tot_runs <<','<< procs[proc_id] << "\n";
+        fout << fixed << setprecision(12) << file_name[itr] << ',' << sizeofmat[itr] << ',' <<
+        cntb << ',' << cnta <<','<< (tot_time*pow(10,-9))/tot_runs <<','<< nthreads << "\n";
 
     
 
